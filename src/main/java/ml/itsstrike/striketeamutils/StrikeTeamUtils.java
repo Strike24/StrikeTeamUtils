f60cc384c6c977abf8cc32fe2aae832345364114
Strike24 <benxcohen@gmail.com>
2023-04-11 13:08:12 +0300

    First Commit ❗

---
+++ src/main/java/ml/itsstrike/striketeamutils/StrikeTeamUtils.java
@@ -0,0 +1,17 @@
+package ml.itsstrike.striketeamutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StrikeTeamUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
