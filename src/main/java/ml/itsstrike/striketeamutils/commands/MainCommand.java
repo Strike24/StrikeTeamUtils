61209f8bc0f221c09278e32050865fa1ece456ff
Strike24 <benxcohen@gmail.com>
2023-04-19 17:23:42 +0300

    Revert: reverted the last merge, was a mistake

--- src/main/java/ml/itsstrike/striketeamutils/commands/MainCommand.java
+++ src/main/java/ml/itsstrike/striketeamutils/commands/MainCommand.java
@@ -5,10 +5,12 @@ import ml.itsstrike.striketeamutils.TeamManager;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Team;
 import revxrsal.commands.annotation.AutoComplete;
 import revxrsal.commands.annotation.Command;
+import revxrsal.commands.annotation.DefaultFor;
 import revxrsal.commands.annotation.Subcommand;
 import revxrsal.commands.bukkit.BukkitCommandActor;
 import revxrsal.commands.bukkit.EntitySelector;
@@ -148,6 +150,46 @@ public class MainCommand {
             }
         });
         actor.getSender().sendMessage("§aTeam §f" + teamName + "§a has been teleported to §f" + player.getName() + "§a.");
+    }
+    @DefaultFor({"teamutils", "teamu", "tu"})
+    @Subcommand("help")
+    public void helpCommand(BukkitCommandActor actor) {
+        CommandSender sender = actor.getSender();
+        sender.sendMessage("§aStrikeTeamUtils Help:");
+        sender.sendMessage("§8- §f/teamutils createteam <teamName> <colorName>");
+        sender.sendMessage("§8- §f/teamutils list");
+        sender.sendMessage("§8- §f/teamutils listplayers <teamName>");
+        sender.sendMessage("§8- §f/teamutils removeteam <teamName>");
+        sender.sendMessage("§8- §f/teamutils addPlayer <teamName> <playerName>");
+        sender.sendMessage("§8- §f/teamutils removePlayer <teamName> <playerName>");
+        sender.sendMessage("§8- §f/teamutils organizePlayers");
+        sender.sendMessage("§8- §f/teamutils makeGlow <teamName>");
+        sender.sendMessage("§8- §f/teamutils makeNotGlow <teamName>");
+        sender.sendMessage("§8- §f/teamutils tpTeam <teamName> <playerName>");
+        sender.sendMessage("§8- §f/teamutils help");
+        sender.sendMessage("§8- §fWiki -§b https://github.com/Strike24/StrikeTeamUtils/wiki/StrikeTeamUtils-Official-Wiki-%F0%9F%93%96");
+    }
+
+    @Subcommand("runcommand")
+    @AutoComplete("@teamNames")
+    public void runCommandAsTeam(BukkitCommandActor actor, String teamName, String command) {
+        if (command.startsWith("/")) {
+            actor.getSender().sendMessage("§cPlease don't use / in the command.\n§aExample: §fkill %player% §cinstead of §f/kill %player%");
+            return;
+        }
+        if (!TeamManager.teamExist(teamName)) {
+            actor.getSender().sendMessage("§cTeam doesn't exists. Please check the name again!\n§aTeams:");
+            TeamManager.listTeams().forEach(team -> {
+                actor.getSender().sendMessage("§8- §f" + team.getName());
+            });
+            return;
+        }
+        TeamManager.getTeam(teamName).getPlayers().forEach(player -> {
+            if (player.isOnline()) {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replace("%player%", player.getName()));
+            }
+        });
+        actor.getSender().sendMessage("§aCommand §f" + command + "§a has been executed as team §f" + teamName + "§a.");
     }
 
 
